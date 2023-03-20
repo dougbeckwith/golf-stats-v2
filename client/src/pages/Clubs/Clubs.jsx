bb017c530c04b6de2ff9188b0ac54c5e04e72341
Doug Beckwith <dougcbeckwith@gmail.com>
2023-03-20 10:54:25 -0700

    remove auth clubs

--- client/src/pages/Clubs/Clubs.jsx
+++ client/src/pages/Clubs/Clubs.jsx
@@ -5,12 +5,12 @@ import axios from 'axios'
 import ClubItem from './ClubItem'
 import ClubList from './ClubList'
 import {sortClubsByAvgYards} from '../../helpers'
-import useAuth from '../../hooks/useAuth'
+// import useAuth from '../../hooks/useAuth'
 
 const Clubs = () => {
   const navigate = useNavigate()
 
-  const {auth} = useAuth()
+  // const {auth} = useAuth()
 
   const [clubData, setClubData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
@@ -21,11 +21,11 @@ const Clubs = () => {
       try {
         const response = await axios.get(
           `${process.env.CYCLIC_URL}/api/clubs`,
-          {
-            headers: {
-              Authorization: 'Bearer ' + auth.accessToken, //the token is a variable which holds the token
-            },
-          }
+          // {
+          //   headers: {
+          //     Authorization: 'Bearer ' + auth.accessToken, //the token is a variable which holds the token
+          //   },
+          // }
         )
         const data = sortClubsByAvgYards(response.data)
         let highestAvgShot = data[0].avgYards
@@ -37,10 +37,11 @@ const Clubs = () => {
       }
     }
     console.log(auth)
-    if (!Object.keys(auth).length === 0) {
-      console.log(auth)
-      getAllClubData()
-    }
+    // if (!Object.keys(auth).length === 0) {
+    //   console.log(auth)
+    //   getAllClubData()
+    // }
+    getAllClubData()
 
     // eslint-disable-next-line
   }, [auth])
